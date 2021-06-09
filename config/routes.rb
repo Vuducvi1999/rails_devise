4108fb68eb02fb7d83fea33abcce164f5e4328db
Vuducvi1999 <vuducvi20@gmail.com>
2021-06-09 18:00:51 +0700

    thêm info account

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to:'home#index'
   devise_for :users, controllers: { confirmations: 'confirmations' }
-  get '/content_sample', to:'home#content_sample'
+
+  resource :home, controller: :home, only: [:index] do
+    collection do
+      get :content_sample
+    end
+  end
+
+  resource :user, controller: :user, only: [:show]
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email:'vuducvi20@gmail.com', password:'anhvipandan', password_confirmation:'anhvipandan')
+User.update_all confirmed_at: Time.now
