1fceffab06d161a3f15a5b4f3592a708f3af9309
Vuducvi1999 <vuducvi20@gmail.com>
2021-06-09 15:23:43 +0700

    custom confirmations controller devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to:'home#index'
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   get '/content_sample', to:'home#content_sample'
 end
