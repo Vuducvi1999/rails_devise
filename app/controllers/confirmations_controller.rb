16f240df114466d8de28c41bea8108fe2d54a187
Vuducvi1999 <vuducvi20@gmail.com>
2021-06-09 23:19:58 +0700

    redirect path after sign in

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -2,10 +2,6 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
     sign_in(resource) # In case you want to sign in the user
-    print "resource_name la gi"
-    puts resource_name
-    print "resource la gi"
-    puts resource
     root_path
     # your_new_after_confirmation_path
   end
