5cfcffdc67b04789bff1c6e77389268e726283c5
Vuducvi1999 <vuducvi20@gmail.com>
2021-06-09 13:26:51 +0700

    done config bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap'
+import '../stylesheets/application'
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap'
-require("stylesheets/application.scss")
