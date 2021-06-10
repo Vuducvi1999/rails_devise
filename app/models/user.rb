95d3c208762ea835292dd5967c011844f7bd1073
Vuducvi1999 <vuducvi20@gmail.com>
2021-06-10 21:28:37 +0700

    add timeoutable

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          # thêm tính năng
          :confirmable, :trackable, :lockable, 
+         :timeoutable,
          :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
 
   def self.from_omniauth(access_token)
